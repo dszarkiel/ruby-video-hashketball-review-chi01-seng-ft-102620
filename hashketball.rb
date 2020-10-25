94a3bed1e691f25cd4b42c040baf15ed2c7bed4a
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-25 17:22:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -178,5 +178,7 @@ def player_numbers(team_name)
 end
 
 def player_stats(player_name)
-  players[:player_name]
+  players.each do |key|
+    if key[:player_name] == player_name
+      return key
 end
