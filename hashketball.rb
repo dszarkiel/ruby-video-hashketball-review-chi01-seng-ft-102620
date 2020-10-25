210850d54b849fef9343986706070a4399ff3cc0
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-10-25 16:27:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -129,9 +129,8 @@ end
 
 def players
   game_hash[:home][:players].merge(game_hash[:home][:players])
-  binding.pry
 end
-
+binding.pry
 def num_points_scored(player_name)
   players[player_name][:points]
 end
